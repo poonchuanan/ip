bebdf32e2ac887116ae21d5114ab57d2c07955f7
poonchuanan <poonchuanan@gmail.com>
2020-09-06 18:55:47 +0800

    edit if-else format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,6 +30,7 @@ public class Duke {
             return "[T][" + getStatusIcon() + "] " + super.toString();
         }
     }
+
     public static class Deadline extends Task{
         protected String by;
         public Deadline(String description, String by){
@@ -71,50 +72,46 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }
-            else if (input.equals("list")){
+            } else if (input.equals("list")){
                 System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i<itemsCount; i++){
 //                    System.out.println(i+1 + ".["+ items[i].getStatusIcon() +"] "+ items[i].description);
                     System.out.println(String.valueOf(i+1) + ". " + items[i]);
                 }
                 input = in.nextLine();
-            }
-            else if (input.contains("done")){
+            } else if (input.contains("done")){
                 int taskNumber = Integer.parseInt(input.replaceAll("[^0-9]", ""));
                 items[taskNumber-1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("[" + items[taskNumber-1].getStatusIcon()+ "] " + items[taskNumber-1].description);
                 input = in.nextLine();
-            }
-            else {
+            } else {
                 if (input.contains("todo")){
                     input = input.replace("todo ","");
                     Todo t = new Todo(input);
                     items[itemsCount] = t;
-                }
-                else if (input.contains("deadline")){
+                    System.out.println("Got it. I've added this task:\n"+ t);
+                } else if (input.contains("deadline")){
                     int indexDivider = input.indexOf("/by");
                     String dueDate = input.substring(indexDivider+3);
                     input = input.substring(0,indexDivider).replace("deadline ", "");
                     Deadline t = new Deadline(input, dueDate);
                     items[itemsCount] = t;
-                }
-                else if (input.contains("event")){
+                    System.out.println("Got it. I've added this task:\n"+ t);
+                } else if (input.contains("event")){
                     int indexDivider = input.indexOf("/at");
                     String eventTime = input.substring(indexDivider+3);
                     input = input.substring(0,indexDivider).replace("event ", "");
                     Event t = new Event(input, eventTime);
                     items[itemsCount] = t;
-                }
-                else{
+                    System.out.println("Got it. I've added this task:\n"+ t);
+                } else{
                     System.out.println("Invalid command");
                     input = in.nextLine();
                     continue;
                 }
                 itemsCount++;
-                System.out.println("Got it. I've added this task:\n"+ input);
-                System.out.println("You now have " + itemsCount + " task(s) in the list.");
+                System.out.println("Now you have " + itemsCount + " task(s) in the list.");
                 input = in.nextLine();
             }
         }
