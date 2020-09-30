ef0973f1c017aa84346f51acad7bfbc2895ec9c4
poonchuanan <poonchuanan@gmail.com>
2020-09-30 21:47:10 +0800

    add javadoc

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,19 @@
 public class Event extends Task{
     protected String at;
+
+    /**
+     * Represents the properties of Event task.
+     */
     public Event(String description, String at){
         super(description);
         this.at = at;
     }
+
+    /**
+     * Stores details of event
+     *
+     * @return full details of task -  event type, completion status and description
+     */
     @Override
     public String toString() {
         return "[E][" + getStatusIcon() + "] " + super.toString() + "(at:" + at + ")";
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,7 +1,20 @@
+/**
+ * Represents the properties of Todo task.
+ */
 public class Todo extends Task{
-    public Todo(String description){
+    /**
+     * Stores details of Todo
+     * @param description description of task
+     */
+    public Todo(String description) {
         super(description);
     }
+
+    /**
+     * Stores details of Todo
+     *
+     * @return full details of task -  todo type, completion status and description
+     */
     @Override
     public String toString() {
         return "[T][" + getStatusIcon() + "] " + super.toString();
